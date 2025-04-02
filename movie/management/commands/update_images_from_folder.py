6b103d54b2f84606812d16a25526fb0bdd6f6ac2
MariaAcevedo11 <macevedos2@eafit.edu.co>
2025-04-02 09:11:24 -0500

    Avance, falta caracteres especiales

--- movie/management/commands/update_images_from_folder.py
+++ movie/management/commands/update_images_from_folder.py
@@ -4,41 +4,26 @@ from django.core.management.base import BaseCommand
 from movie.models import Movie
 
 class Command(BaseCommand):
-    help = ""
+    help = "Update images :D"
 
     def handle(self, *args, **kwargs):
         # 📥 Ruta del archivo CSV con las descripciones actualizadas
-        csv_file = 'updated_movie_descriptions.csv'  # ← Puedes cambiar el nombre si es necesario
-
-        # ✅ Verifica si el archivo existe
-        if not os.path.exists(csv_file):
-            self.stderr.write(f"CSV file '{csv_file}' not found.")
-            return
-
         updated_count = 0
 
-        # 📖 Abrimos el CSV y leemos cada fila
-        with open(csv_file, mode='r', encoding='utf-8') as file:
-            reader = csv.DictReader(file)
-            for row in reader:
-                title = row['Title']
-                new_description = row['Updated Description']
+    # 📖 Abrimos el CSV y leemos cada fila
+        
+        for movie in Movie.objects.all():
+            if not os.path.exists("./media/movie/images/m_" + movie.title + ".png"):
+                self.stderr.write(f"Image file  'm_{movie.title}.png' not found.")
+            else:
+                movie.image = "movie/images/m_" + movie.title + ".png"
+                movie.save()
+                updated_count += 1
+                self.stdout.write(self.style.SUCCESS(f"Updated: {movie.title} image succesfully loaded"))
 
-                try:
-                    # ❗ Aquí debes completar el código para buscar la película por título
-                    movie = Movie.objects.get(title = title)
+    # ✅ Al finalizar, muestra cuásntas películas se actualizaron
+        self.stdout.write(self.style.SUCCESS(f"Finished updating {updated_count} movies image from Images Folder."))
 
-                    # ❗ Aquí debes actualizar la descripción de la película
-                    movie.description = new_description
-                    movie.save()
-                    updated_count += 1
 
-                    self.stdout.write(self.style.SUCCESS(f"Updated: {title}"))
-
-                except Movie.DoesNotExist:
-                    self.stderr.write(f"Movie not found: {title}")
-                except Exception as e:
-                    self.stderr.write(f"Failed to update {title}: {str(e)}")
-
-        # ✅ Al finalizar, muestra cuántas películas se actualizaron
-        self.stdout.write(self.style.SUCCESS(f"Finished updating {updated_count} movies from CSV."))
+    def specialCharacter(self): 
+        ()
